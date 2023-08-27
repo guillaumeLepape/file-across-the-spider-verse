535306f5c99255bb156cc27b36a615fcf711c212
Guillaume Le Pape <gui.lepape25@gmail.com>
2023-08-27 22:09:53 +0200

    Start integration of sqlite

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"net"
+	"path/filepath"
 
+	"github.com/guillaumeLepape/files-across-the-spider-verse/database"
 	"github.com/guillaumeLepape/files-across-the-spider-verse/filehandling"
 )
 
@@ -10,4 +14,18 @@ func main() {
 	spiderVersePath := filehandling.GetSpiderVersePath()
 
 	fmt.Println("Spider verse path:", spiderVersePath)
+
+	db := database.Connect(filepath.Join(spiderVersePath, ".spider_metadata.db"))
+
+	err := db.AutoMigrate(&database.Host{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.Create(
+		&database.Host{
+			Name: fmt.Sprint("Machine", rand.Intn(1000)),
+			IP:   database.CustomIP(net.IPv4(10, 255, 0, byte(rand.Intn(100)))),
+		},
+	)
 }
