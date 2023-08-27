7446890a00a28a8aa5f852d677a87f71a1dde36b
Guillaume Le Pape <gui.lepape25@gmail.com>
2023-08-27 18:15:40 +0200

    Create a separate module for file handling

--- main.go
+++ main.go
@@ -2,53 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
+
+	"github.com/guillaumeLepape/files-across-the-spider-verse/filehandling"
 )
 
-func CreateFileContent(dirname string, fileLocation string) string {
-	return fmt.Sprint(filepath.Join(dirname, fileLocation), "\n")
-}
-
 func main() {
-	homedir, err := os.UserHomeDir()
+	spiderVerseLocation, err := filehandling.GetSpiderVerseLocation()
+
 	if err != nil {
-		log.Fatal(err)
+		spiderVerseLocation = filehandling.CreateFile()
 	}
 
-	filename := fmt.Sprint(filepath.Join(homedir, ".spider_file"))
-
-	fi, err := os.Lstat(filename)
-
-	if os.IsNotExist(err) {
-		var fileLocation string
-		fmt.Print("Input a string: ")
-		fmt.Scanln(&fileLocation)
-
-		err := os.WriteFile(filename, []byte(CreateFileContent(homedir, fileLocation)), 0666)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		if fi.Mode().IsRegular() {
-			data, err := os.ReadFile(filename)
-			if err != nil {
-				log.Fatal("Could not open file")
-			}
-
-			spiderVerseLocation := strings.Replace(string(data), "\n", "", -1)
-
-			fmt.Println("Spider verse location", spiderVerseLocation)
-
-			mkdirErr := os.MkdirAll(spiderVerseLocation, os.ModePerm)
-			if mkdirErr != nil {
-				log.Fatal("Not able to create the folder")
-			}
-		} else {
-			log.Fatal("Is not a regular file")
-		}
-	}
+	fmt.Println("Spider verse location", spiderVerseLocation)
 }
