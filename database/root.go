6344311c404b248083ef27aa575b176e379d0e44
Guillaume Le Pape <gui.lepape25@gmail.com>
2023-08-27 22:09:53 +0200

    Start integration of sqlite

---
+++ database/root.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Host struct {
+	ID   uint     `gorm:"primaryKey;autoIncrementIncrement:true"`
+	Name string   `gorm:"uniqueIndex"`
+	IP   CustomIP `gorm:"unique"`
+}
+
+func Connect(file string) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(file), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	return db
+}
